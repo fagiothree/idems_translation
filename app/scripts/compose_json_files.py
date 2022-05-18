168c9c961f7f5327125c5f04011c6aaa2c7847bf
Ian Stride <ianstride@gmail.com>
2022-05-18 15:07:22 +0100

    Allow 'compose' script to receive input/output dirs from command line

--- app/scripts/compose_json_files.py
+++ app/scripts/compose_json_files.py
@@ -1,35 +1,52 @@
 import json
 import os
+import sys
+from pathlib import Path
 
 
-def create_list(input_name, subfolders = []):
-    input_list = []
-    dirpath = app_path + input_name + '/'
-    for file_name in os.listdir(dirpath):
-        if os.path.isfile(os.path.join(dirpath, file_name)):
-            json_file = json.load(open(dirpath + file_name, encoding='utf-8'))
-            input_list.append(json_file)
-    
-    
-    for subf in subfolders:
-        for file_name in os.listdir(dirpath + subf + "/"):
-            if os.path.isfile(os.path.join(dirpath + subf + "/", file_name)):
-                json_file = json.load(open(dirpath+ subf + "/" + file_name, encoding='utf-8'))
-                input_list.append(json_file)
-            
+def main():
+    in_dir = Path(sys.argv[1])
+    out_dir = Path(sys.argv[2])
+    out_dir.mkdir(parents=True, exist_ok=True)
 
-    with open(output_folder + '/input_' + input_name + '.json', 'w', encoding='utf-8') as json_file:
-        json.dump(input_list, json_file, ensure_ascii=False, indent=2)
+    groups = [
+        ('data_list', ['campaign_rows']),
+        ('global', []),
+        ('template', []),
+        ('tour', []),
+    ]
+
+    for (name, sub_dirs) in groups:
+        contents = create_list(in_dir, name, sub_dirs)
+        write_json(out_dir, name, contents)
+
+def create_list(in_dir, group_name, subfolders):
+    input_list = accumulate_json_files(in_dir / group_name)
+
+    for folder in subfolders:
+        input_list += accumulate_json_files(in_dir / group_name / folder)
+
+    return input_list
+
+def accumulate_json_files(dir_path):
+    acc = []
+
+    for file_name in os.listdir(dir_path):
+        file_path = dir_path / file_name
+        if file_path.is_file():
+            acc.append(read_json(file_path))
+
+    return acc
+
+def read_json(file_path):
+    with open(file_path, encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+def write_json(out_dir, group_name, contents):
+    file_path = out_dir / f'input_{group_name}.json'
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(contents, json_file, ensure_ascii=False, indent=2)
+
 
 if __name__ == '__main__':
-    output_folder = './input'
-
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-
-    app_path = "../../parenting-app-ui/packages/app-data/sheets/"
-    create_list("data_list", ["campaign_rows"])
-    create_list("global")
-    create_list("template")
-    create_list("tour")
+    main()
