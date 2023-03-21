c23e32bd12f5f89581a567bfde03a2cc87d7ea68
Ed Moss <ed.moss@eemengineering.com>
2023-03-21 16:17:18 -0300

    changes to merge file process

--- app/scripts/merge_files_post_translation.py
+++ app/scripts/merge_files_post_translation.py
@@ -11,17 +11,14 @@ def main():
     with open(config_path) as config_file:
         data = json.load(config_file)
 
-        split_groups = data['SplitGroups']
         post_translation = data['PostTranslation']
         post_translation_jsons = data['PostTranslationJSONs']
         final_dictionaries = data['FinalDictionaries']
 
-    # Loop through the custom groups that we sent to the translators
-    for group_info in split_groups:
-        foldername = group_info[1]
-        folderpath = os.path.join(post_translation, foldername)
+    # Loop through files where the translations are stored
+    for folderpath in post_translation:
 
-        # For each of these groups loop through the language folders that we got back
+        # For each of these folders loop through the language folders that we got back
         for folder in os.listdir(folderpath):
             full_lang_path = os.path.join(folderpath, folder)
             lang_name = os.path.basename(folder)
